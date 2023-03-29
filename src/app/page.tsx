64e6ef69a367c7bd5fde7b7ba99e11465508769b
DevilsDomain <alaraerzincan@gmail.com>
2023-03-29 13:44:42 +0200

    front page: aligned the buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,20 +29,21 @@ export default function Home() {
           <Image src={homeBottom} width={300} height={380} alt='blob top' 
           className='blur-3xl absolute z-0 image-size p-14'/>
       </div>
-      <h2 className='z-10 relative text-base font-light	leading-5 mt-20	mb-5 md:text-3xl 
-      md:font-extralight md:mr-24'
+      <h2 className='z-10 relative text-base font-light	leading-5 mt-20	mb-5 md:text-xl 
+      md:font-extralight md:-ml-96'
       >MEET YOUR FUTURE DEVELOPER</h2>
       <div className="relative flex flex-col items-center md:items-start	">
-        <div className='flex flex-col items-center md:flex md:flex-row md:-ml-20'>
+        <div className='flex flex-col items-center md:flex md:flex-row md:-ml-36'>
           <h1 className='z-10 relative text-6xl	font-bold
-          md:text-[13rem]
+          md:text-[11rem]
           '>SATI</h1>
           <Image src={star} width={50} height={50} alt='star' className="z-0 relative	md:w-44	md:h-44 md:pl-4" />
         </div>
         <h1 className='z-10 relative text-6xl	font-bold mb-10	
-        md:text-[13rem] md:ml-64
+        md:text-[11rem] md:ml-40 md:mb-6
         '>ALARA</h1>
-        <div className='flex flex-col flex-wrap w-64 gap-y-5	items-center '>
+        <div className='flex flex-col flex-wrap w-64 gap-y-10	items-center
+        md:w-80	md:flex-row md:gap-5 md:scale-110	md:ml-96'>
           <Button type='primary' link='contact'>CONTACT</Button>
           <Button type="secondary" link='recent-work'>RECENT WORK</Button>
         </div>
