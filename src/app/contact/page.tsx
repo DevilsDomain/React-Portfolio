88fb00af18461975524d6d529b0b9f20725eb40b
DevilsDomain <alaraerzincan@gmail.com>
2023-03-29 20:49:36 +0200

    contact page: desktop view done

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -15,9 +15,10 @@ function page() {
           <Image src={contactBottom} width={550} height={550} alt='blob top' 
           className='blur-3xl absolute z-0 bottom-0 left-0 mr-40 zindex-1'/>
        </div>
-       <div className='relative h-screen w-11/12 flex flex-col items-center justify-center'>
+       <div className='relative h-screen w-11/12 flex flex-col items-center justify-center
+       md:w-3/6	'>
         <Image src={star} width={100} height={100} alt='star' className="z-0 relative" />
-        <form className='flex flex-col w-11/12'>
+        <form className='flex flex-col w-11/12 md:w-3/6	'>
             <label htmlFor='name' className='text-2xl mb-8 mt-10'>NAME</label>
             <input id='name' name='name' placeholder='name' 
             className='bg-transparent placeholder:text-slate-900 placeholder:font-light	 
@@ -26,7 +27,7 @@ function page() {
             <input id='email' name='email' placeholder='exampple@email.com' 
             className='bg-transparent placeholder:text-slate-900 border-b 
             border-slate-900 placeholder:font-light' />
-            <label htmlFor='idea' className='text-2xl mb-8 mt-10'>IDEA</label>
+            <label htmlFor='idea' className='text-2xl mb-8 mt-12'>IDEA</label>
             <input id='idea' name='idea' placeholder='I have this crazy idea...' 
             className='bg-transparent placeholder:text-slate-900 border-b border-slate-900 placeholder:font-light' />
             <div className='flex flex-col mt-10 items-center'>
