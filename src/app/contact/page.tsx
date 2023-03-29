2658dba932767f39c591a8202550dfa7cf971e5b
DevilsDomain <alaraerzincan@gmail.com>
2023-03-29 20:46:21 +0200

    contact page mobile view done

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -15,16 +15,23 @@ function page() {
           <Image src={contactBottom} width={550} height={550} alt='blob top' 
           className='blur-3xl absolute z-0 bottom-0 left-0 mr-40 zindex-1'/>
        </div>
-       <div className='relative flex flex-col items-center'>
+       <div className='relative h-screen w-11/12 flex flex-col items-center justify-center'>
         <Image src={star} width={100} height={100} alt='star' className="z-0 relative" />
-        <form className='flex flex-col'>
-            <label htmlFor='name'>NAME</label>
-            <input id='name' name='name' placeholder='name' />
-            <label htmlFor='email'>EMAIL</label>
-            <input id='email' name='email' placeholder='exampple@email.com' />
-            <label htmlFor='idea'>IDEA</label>
-            <input id='idea' name='idea' placeholder='I have this crazy idea...' />
-            <Button type='primary'>SEND</Button>
+        <form className='flex flex-col w-11/12'>
+            <label htmlFor='name' className='text-2xl mb-8 mt-10'>NAME</label>
+            <input id='name' name='name' placeholder='name' 
+            className='bg-transparent placeholder:text-slate-900 placeholder:font-light	 
+            border-b border-slate-900	' />
+            <label htmlFor='email' className='text-2xl mb-8 mt-10'>EMAIL</label>
+            <input id='email' name='email' placeholder='exampple@email.com' 
+            className='bg-transparent placeholder:text-slate-900 border-b 
+            border-slate-900 placeholder:font-light' />
+            <label htmlFor='idea' className='text-2xl mb-8 mt-10'>IDEA</label>
+            <input id='idea' name='idea' placeholder='I have this crazy idea...' 
+            className='bg-transparent placeholder:text-slate-900 border-b border-slate-900 placeholder:font-light' />
+            <div className='flex flex-col mt-10 items-center'>
+                <Button type='primary'>SEND</Button>
+            </div>
         </form> 
        </div>
     </div>
