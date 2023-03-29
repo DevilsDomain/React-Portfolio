b074614e53628e8ac8398e38cff465bdbd37abe3
DevilsDomain <alaraerzincan@gmail.com>
2023-03-29 12:06:28 +0200

    navbar desktop view links fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,7 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <div className="relative flex flex-col items-center">
-      <div className='hidden md:block'>
+      <div className='hidden md:block z-40'>
             <div className='flex flex-row gap-100 pt-4'>
                 <Link className="text-sm text-stone-900 font-medium" href={'/'}>HOME</Link>
                 <Link className="text-sm text-stone-900 font-medium" href={'/about'}>ABOUT</Link>
@@ -29,7 +29,9 @@ export default function Home() {
           <Image src={homeBottom} width={300} height={380} alt='blob top' 
           className='blur-3xl absolute z-0 image-size p-14'/>
       </div>
-      <h2 className='z-10 relative text-base font-light	leading-5 mt-20	mb-5'>MEET YOUR FUTURE DEVELOPER</h2>
+      <h2 className='z-10 relative text-base font-light	leading-5 mt-20	mb-5 md:text-3xl 
+      md:font-extralight'
+      >MEET YOUR FUTURE DEVELOPER</h2>
       <div className="relative flex flex-col items-center">
         <h1 className='z-10 relative text-6xl	font-bold'>SATI</h1>
         <Image src={star} width={63} height={71} alt='star' className="z-0 relative" />
