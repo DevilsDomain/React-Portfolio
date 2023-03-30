a88379bc4ddbd9b773be223e699976f875dcdc94
DevilsDomain <alaraerzincan@gmail.com>
2023-03-30 16:22:38 +0200

    projects: mobile view done

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -7,7 +7,7 @@ import Backend from './Tags/Backend'
 
 function Project() {
   return (
-    <div className='flex flex-col items-center'>
+    <div className='flex flex-col items-center mt-32'>
         <div className='flex flex-col items-center'>
             <h1 className='text-white text-[7rem] font-bold'>01</h1>
             <h2 className='text-white text-2xl text-justify mb-2'>PROJECT DESCRIPTION</h2>
@@ -29,7 +29,7 @@ function Project() {
             <h1 className='text-white text-3xl text-justify mt-10'>PROJECT TITLE</h1>
             <Image src={'https://cdn.dribbble.com/userupload/5817809/file/original-5de0b1e4bd364e5845f80c46c7310fb1.png'} 
             alt='project name' width={300} height={300}/>
-            <div className='flex flex-row scale-50	'>
+            <div className='flex flex-row scale-50 gap-x-24 mr-10'>
                 <UI mode='dark' />
                 <Frontend mode='dark' />
                 <Backend mode='dark'/>
