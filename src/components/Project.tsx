939dbd4dc8f7d926d6920712411b4e92932953bc
DevilsDomain <alaraerzincan@gmail.com>
2023-03-30 18:00:15 +0200

    projects page: mobile view fixed

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -7,10 +7,10 @@ import Backend from './Tags/Backend'
 
 function Project() {
   return (
-    <div className='flex flex-col items-center mt-32 md:flex-row md:tems-start'>
+    <div className='flex flex-col items-center mt-32 md:flex-row md:tems-start overflow-x-hidden'>
         <div className='flex flex-col items-center md:items-start md:-mt-60'>
             <h1 className='text-white text-[7rem] font-bold md:ml-20'>01</h1>
-            <h2 className='text-white text-4xl text-justify mb-2 md:ml-20'>PROJECT DESCRIPTION</h2>
+            <h2 className='text-white text-3xl text-justify mb-2 md:ml-20 md:text-center'>PROJECT DESCRIPTION</h2>
             <div className='mx-16 md:mx-20'>
                 <p className='text-white text-justify text-sm mb-10 md:text-base md:w-96'>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. 
                 Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt 
@@ -28,7 +28,9 @@ function Project() {
         <div className='flex flex-col items-center mb-20 md:mr-10'>
             <h1 className='text-white text-3xl text-justify mt-10 md:text-8xl'>PROJECT TITLE</h1>
             <Image src={'https://cdn.dribbble.com/userupload/5817809/file/original-5de0b1e4bd364e5845f80c46c7310fb1.png'} 
-            alt='project name' width={900} height={900} className=''/>
+            alt='project name' width={900} height={900} className='hidden md:block'/>
+            <Image src={'https://cdn.dribbble.com/userupload/5817809/file/original-5de0b1e4bd364e5845f80c46c7310fb1.png'} 
+            alt='project name' width={350} height={350} className='md:hidden'/>
             <div className='flex flex-row scale-50 gap-x-24 mr-10'>
                 <UI mode='dark' />
                 <Frontend mode='dark' />
