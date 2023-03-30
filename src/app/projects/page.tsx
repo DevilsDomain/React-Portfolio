40b01a548d46aa6146e07d1d22d3eaaa810cac0a
DevilsDomain <alaraerzincan@gmail.com>
2023-03-30 15:20:03 +0200

    projects page: mobile view initial page done

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,15 +1,21 @@
 import React from 'react'
 import ProjectLayout from '@/components/ProjectsLayout'
 import 'src/app/projects/page.css'
+import Image from 'next/image'
+import explore from '../../../public/explore.svg'
 
 
 function page() {
   return (
     <ProjectLayout>
-        <div className='background h-screen w-screen'>
-            <p className='text-white'>hello</p>
+    <div className='background flex flex-col justify-center'>
+        <div className='border-b border-t border-gray-300 
+        text-[5rem] leading-none mt-20 font-bold mb-10 md:text-[8rem] md:ml-52'>
+            <h1 className='mb-10 mt-28 w-28 ml-2 text-white'>EXPLORE MY WORK</h1>
+            <Image src={explore} height={200} width={200} alt='graphic icon' className='mb-28 ml-2'/>
         </div>
-    </ProjectLayout>
+    </div>
+</ProjectLayout>
   )
 }
 
