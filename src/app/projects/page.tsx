c9c00b9bc99b3cf97df27389ae1e8f7ace465eb0
DevilsDomain <alaraerzincan@gmail.com>
2023-03-30 22:36:27 +0200

    fixed some styles

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -13,25 +13,26 @@ import linkedin from '../../../public/linkedin.svg'
 import { getClient } from '../../../ApolloClient'
 import {gql} from '@apollo/client'
 
+
 const PROJECTS_DATA = gql`
 query MyQuery {
   projectDatas {
+    ui
     backend
     frontend
     imageUrl
     projectDescription
     title
-    updatedAt
   }
 } `;
 
 async function page() {
   const client = getClient();
-  const { data } = await client.query({query: PROJECTS_DATA });
+  const { data } = await client.query({query: PROJECTS_DATA});
   console.log(data)
   return (
     <ProjectLayout>
-    <div className='background flex flex-col justify-center'>
+    <div className='background flex flex-col justify-center overflow-x-hidden'>
         <div className='border-b border-t border-gray-300 md:flex md:flex-row
         text-[5rem] leading-none mt-14 font-bold mb-10 md:text-[12rem]'>
             <h1 className='mb-10 mt-28 w-28 ml-2 text-white md:w-full md:ml-20'>EXPLORE MY WORK
@@ -39,7 +40,9 @@ async function page() {
             <Image src={explore} height={200} width={200} alt='graphic icon' className='mb-28 ml-2 md:mr-20
             md:scale-105'/>
         </div>
-        <Project />
+        {data.projectDatas.map((project, projectId) => {
+          return(<Project key={projectId} id={projectId} data={project}/>);
+        })}
         <div className='flex flex-col items-center border-t border-gray-30 mb-32 md:scale-110'>
             <Image src={star} width={100} height={100} alt='star icon'className='invert scale-75 mt-10 mb-10'/>
             <p className='text-white w-28 text-justif mb-10'>HAVE AN IDEA? LETS MAKE IT A REALITY.</p>
--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -5,36 +5,30 @@ import UI from './Tags/UI'
 import Frontend from './Tags/Frontend'
 import Backend from './Tags/Backend'
 
-function Project() {
+function Project({data, id}) {
   return (
     <div className='flex flex-col items-center mt-32 md:flex-row md:tems-start overflow-x-hidden'>
         <div className='flex flex-col items-center md:items-start md:-mt-60'>
-            <h1 className='text-white text-[7rem] font-bold md:ml-20'>01</h1>
+            <h1 className='text-white text-[7rem] font-bold md:ml-20'>{id < 9 ? `0${id+1}` : `${id}`}</h1>
             <h2 className='text-white text-3xl text-justify mb-2 md:ml-20 md:text-center'>PROJECT DESCRIPTION</h2>
             <div className='mx-16 md:mx-20'>
-                <p className='text-white text-justify text-sm mb-10 md:text-base md:w-96'>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. 
-                Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt 
-                nostrud amet.</p>
-                <p className='text-white text-justify text-sm md:text-base md:w-96'>
-                Maecenas quam nunc, sagittis non condimentum at, rutrum sit amet eros. Fusce rutrum, 
-                lectus in blandit sagittis, mi tortor ullamcorper mi, vitae vestibulum libero quam a nisi.
-                 In eu mauris et neque sodales porta eu eget dui. Nunc eu quam sit amet justo elementum mollis. 
-                 Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. 
-                 Sed laoreet metus nulla, in gravida urna rhoncus in. Proin laoreet semper tortorsequat purus quis 
-                 lectus maximus fermentum.
-                </p>
+                {data.projectDescription.map((p, pIndex) => {
+                    return(
+                <p key={pIndex} className='text-white text-justify text-sm mb-10 md:text-base md:w-96'>{p}</p>
+                    );
+                })}
             </div>
         </div>
         <div className='flex flex-col items-center mb-20 md:mr-10'>
-            <h1 className='text-white text-3xl text-justify mt-10 md:text-8xl'>PROJECT TITLE</h1>
-            <Image src={'https://cdn.dribbble.com/userupload/5817809/file/original-5de0b1e4bd364e5845f80c46c7310fb1.png'} 
+            <h1 className='text-white text-3xl text-justify  mt-10 md:text-8xl'>{data.title}</h1>
+            <Image src={data.imageUrl} 
             alt='project name' width={900} height={900} className='hidden md:block'/>
-            <Image src={'https://cdn.dribbble.com/userupload/5817809/file/original-5de0b1e4bd364e5845f80c46c7310fb1.png'} 
+            <Image src={data.imageUrl} 
             alt='project name' width={350} height={350} className='md:hidden'/>
             <div className='flex flex-row scale-50 gap-x-24 mr-10'>
-                <UI mode='dark' />
-                <Frontend mode='dark' />
-                <Backend mode='dark'/>
+                {data.ui ? (<UI mode='dark' />) : null}
+                {data.frontend ? (<Frontend mode='dark' />) : null}
+                {data.backend ? (<Backend mode='dark'/>) : null}
             </div>
         </div>
     </div>
